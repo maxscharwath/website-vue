736095a72234bf32904569ee61ca59de8a144004
Maxime Scharwath <maxscharwath@gmail.com>
2022-09-15 21:29:07 +0200

    add: responsive navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,7 @@ const navLinks = [
 
 <template>
   <header
-    class="bg-background/75 fixed top-0 left-0 z-10 flex h-16 w-full justify-end backdrop-blur transition duration-300 ease-out [&.unhooked]:shadow-[0_3px_20px_0_rgba(0,0,0,0.5)]"
+    class="bg-background/75 fixed bottom-0 left-0 z-10 flex h-16 w-full justify-center backdrop-blur transition duration-300 ease-out sm:top-0 sm:justify-end [&.unhooked]:shadow-[0_0_20px_3px_rgba(0,0,0,0.5)]"
     :class="{ unhooked: y > 0 }"
   >
     <nav class="flex">
@@ -39,7 +39,7 @@ const navLinks = [
 li a {
   counter-increment: tab-counter 1;
   &::before {
-    @apply mr-1 text-primary font-mono;
+    @apply mr-1 text-primary font-mono block sm:inline text-center text-xs;
     content: counter(tab-counter, decimal-leading-zero) ".";
   }
 }
