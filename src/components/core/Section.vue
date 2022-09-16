43bf120d9842cd544e4ef91d4516745d00581c2d
Maxime Scharwath <maxscharwath@gmail.com>
2022-09-16 15:53:27 +0200

    fix: responsive padding

--- src/components/core/Section.vue
+++ src/components/core/Section.vue
@@ -12,7 +12,7 @@ useMotion(target, {
 <template>
   <section
     ref="target"
-    class="mx-auto flex flex-col items-start justify-center py-24 sm:max-w-5xl"
+    class="mx-auto flex flex-col items-start justify-center py-12 sm:max-w-5xl sm:py-24"
   >
     <slot />
   </section>
